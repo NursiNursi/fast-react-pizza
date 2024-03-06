30749c9e9cbb7166810260a70585a6524e04ea73
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-03-06 16:16:08 +0700

    Apply Spacing and Border

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Nursi</div>;
+  return <div className="hidden text-sm font-semibold">Nursi</div>;
 }
 
 export default Username;
