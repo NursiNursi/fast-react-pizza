a9688a83e36d8d2a33c7944bf9c3a623eb9f5641
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-03-11 11:33:33 +0700

    Configuring Basic Style and Custom Font

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Nursi</div>;
+  return <div className="hidden text-sm font-semibold md:block">Nursi</div>;
 }
 
 export default Username;
